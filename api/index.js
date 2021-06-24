c821551c74ff3bb6848b976b4ce17ec7cceab5a0
KhoirulAziz23 <84756729+KhoirulAziz23@users.noreply.github.com>
2021-06-25 06:21:38 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,34 +6,71 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1809296672:AAGh81AFbdiKC8kLRt8XKHgIn_C4O9oy214'
 const bot = new TelegramBot(token, {polling: true});
 
 
-// bots
+// Main Menu bots
 bot.onText(/\/start/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+// input requires x,y,z
+state = 0;
+bot.onText(/\/predict/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
-    );   
+        'Masukan Nilai X|Y|Z , contoh 39.588|-33.474|-220.645'
+    );
+    state = 1;
 });
 
+bot.on('message', (msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        x = s[0]
+        y = s[1]
+        z = s[2]
+        model.predict(
+            [
+                parseFloat(s[0]), //string to float
+                parseFloat(s[1]),
+                parseFloat(s[2])
+            ]
+       ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `Nilai M1 yang Di Prediksi adalah ${jres[0]}`
+            );
+            
+            bot.sendMessage(
+                msg.chat.id,
+                `Nilai M2 yang Di Prediksi adalah ${jres[1]}`
+            );
+            
+            
+            bot.sendMessage(
+                msg.chat.id,
+                `Nilai M3 yang Di Prediksi adalah ${jres[2]}`
+            );
+        })
+    }else{
+        state = 0
+    }
+})
+
 // routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
+r.get('/prediction/:x/:y/:z', function(req, res, next) {    
     model.predict(
         [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
+            parseFloat(req.params.x), // string to float
+            parseFloat(req.params.y),
+            parseFloat(req.params.z)
         ]
     ).then((jres)=>{
         res.json(jres);
