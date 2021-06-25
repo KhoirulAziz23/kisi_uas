2487da53bc52227feaf0f6e71e492b46ba15148a
KhoirulAziz23 <84756729+KhoirulAziz23@users.noreply.github.com>
2021-06-25 11:05:02 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,20 +1,27 @@
 const tf = require('@tensorflow/tfjs-node');
 
-function normalized(data){ // i & r
-    i = (data[0] - 12.585) / 6.813882
-    r = (data[1] - 51.4795) / 29.151289
-    return [i, r]
+function normalized(data){ // x,y,z
+    
+    x = (data[0] - 1.262) / 24.18347807
+    y = (data[1] - 1.196) / 22.85995322
+    z = (data[2] - -248.947) / 23.86685378
+    return [x,y,x]
+
 }
 
 function denormalized(data){
     v = (data[0] * 552.6264) + 650.4795
     p = (data[1] * 12153.8) + 10620.5615
     return [v, p]
+    
+    m1 = (data[0] * 32.59359793) + 10.339
+    m2 = (data[1] * 30.54807165) + 9.805
+    m3 = (data[2] * 31.36414005) + 10.153
 }
 
 
 async function predict(data){
-    let in_dim = 2;
+    let in_dim = 3;
     
     data = normalized(data);
     shape = [1, in_dim];
@@ -23,7 +30,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/jst_service/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/KhoirulAziz23/kisi_uas/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
